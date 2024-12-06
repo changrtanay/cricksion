2bdec0b258f89c7bc8a00012dd2b71ec1512942f
Tanay <changrtanay@gmail.com>
2024-12-06 19:34:15 +0530

    1. Deployment

---
+++ script.js
@@ -0,0 +1,26 @@
+async function getMatchData() {
+
+    return await fetch(" ")
+        .then(data => data.json())
+        .then(data => {
+            if (data.status != "success")return;
+
+            const matchesList = data.data;
+
+            if(!matchesList)return [];
+            
+            //add your api key from cricketdata.org
+            // const relevantData = matchesList.filter(match => match.series_id == " ").map(match => `${match.name}, ${match.status}`);
+            const relevantData = matchesList.map(match => `${match.name}, ${match.status}`);
+
+            console.log({relevantData});
+
+            document.getElementById("matches").innerHTML = relevantData.map(match => `<li>${match} </li>`).join('');
+
+            return relevantData;
+
+        })
+        .catch(e => console.log(e));
+}
+
+getMatchData();
